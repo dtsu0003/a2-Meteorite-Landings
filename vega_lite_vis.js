40088109a1da0bb5545a74b37aebb2bde052f56c
Daniel <140371737+dtsu0003@users.noreply.github.com>
2025-10-23 03:31:36 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,23 @@
+var vg_1 = "classtype_freq_chart.vg.json";
+vegaEmbed("#classtype_freq", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "classtype_mass_chart.vg.json";
+vegaEmbed("#classtype_mass", vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "year_freq_chart.vg.json";
+vegaEmbed("#year_freq", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "long_lat_chart.vg.json";
+vegaEmbed("#long_lat", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "hubdist_density_chart.vg.json";
+vegaEmbed("#hubdist_density", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "top_mass_chart.vg.json";
+vegaEmbed("#top_mass", vg_6).then(function(result) {
+}).catch(console.error);
